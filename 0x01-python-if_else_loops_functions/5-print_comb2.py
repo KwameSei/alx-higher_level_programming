ea6b341479b4e337cd4b7a97c0e197044f4f8095
KwameSei <nathanielosei92@gmail.com>
2022-08-24 15:32:29 +0000

    Printing integers 00 to 99 updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 for a in range(0, 100):
-        end = ", "
-        if a == 99:
-            end = "\n"
+    end = ", "
+    if a == 99:
+        end = "\n"
 
-        if a < 10:
-            print(f"0{a}", end=end)
-        else:
-            print("{}".format(a), end=end)
+    if a < 10:
+        print(f"0{a}", end=end)
+    else:
+        print("{}".format(a), end=end)
