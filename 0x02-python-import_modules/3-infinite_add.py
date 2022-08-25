b10ee3cb9ba661b3e8d1ac425db4fe0ac2261fe2
KwameSei <nathanielosei92@gmail.com>
2022-08-25 20:34:57 +0000

    Infinite addition of all command line arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    args = len(argv) - 1
+    sum = 0
+    if args == 0:
+        print("{:d}".format(0))
+    else:
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+        print(f"{sum}")
