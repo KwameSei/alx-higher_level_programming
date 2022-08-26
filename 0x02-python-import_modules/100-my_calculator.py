9b6eb4c803938ff9cd2c2be8b0630b0f626c3772
KwameSei <nathanielosei92@gmail.com>
2022-08-26 01:45:22 +0000

    Calculator throwing exceptions edited

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import sys
 
     n = len(sys.argv) - 1
-    if n != 4:
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
@@ -14,14 +14,14 @@ if __name__ == "__main__":
         operator = sys.argv[2]
         b = int(sys.argv[3])
 
-        if operator == "/":
-            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
+        if operator == "+":
+            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
         elif operator == "-":
             print("{:d} {:s} {:d} = {:d}".format(a, operator, b, sub(a, b)))
         elif operator == "*":
             print("{:d} {:s} {:d} = {:d}".format(a, operator, b, mul(a, b)))
-        elif operator == "+":
-            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
+        elif operator == "/":
+            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
