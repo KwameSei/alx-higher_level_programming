a556d6b4f4f728bb0e8fdd1fa4e28600a778beeb
KwameSei <nathanielosei92@gmail.com>
2022-09-12 23:26:07 +0000

    Dividing two integers and printing the results edited

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         results = a / b
-        print("Inside result: {}". format(results))
+        print("Inside result: {:.1f}". format(results))
     except:
         results = None
         print("Inside result: {}". format(results))
