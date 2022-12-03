35e641997bb9c26c1cc303c4633151588a07701b
KwameSei <nathanielosei92@gmail.com>
2022-12-03 11:25:45 +0000

    Tasks one and two

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+'''
+script that lists all states names beginning with the uppercase N
+from database hbtn_0e_0_usa
+'''
+
+if __name__ == "__main__":
+    start_db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8"
+            )
+
+    cur = start_db.cursor()
+    cur.execute("SELECT id, UPPER(name) FROM states WHERE name LIKE 'N%'")
+
+    rows_query = cur.fetchall()
+    for row in rows_query:
+        print(row)
+    cur.close()
+    start_db.close()
