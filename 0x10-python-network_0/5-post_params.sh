103f6e875f0ef5041e46d798813024b9b95c4894
KwameSei <nathanielosei92@gmail.com>
2022-12-15 19:21:48 +0000

    Post request with parameters edited

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends in a URL request and displays the body
-curl -s POST -d "$1" "email=test@gmail.com&subject=I will always be here for PLD"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
