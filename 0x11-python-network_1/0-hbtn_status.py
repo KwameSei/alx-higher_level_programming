74297038cc2bbdfccb20f5f90668cc60e9ddb6cf
KwameSei <nathanielosei92@gmail.com>
2022-12-16 12:18:14 +0000

    Script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import urllib.request as req
+"""
+    # Script that fetches https://alx-intranet.hbtn.io/status
+    #status
+"""
+
+if __name__ == '__main__':
+    url = req.Request('https://alx-intranet.hbtn.io/status')
+    with req.urlopen(url) as res:
+        html = res.read()
+
+        print("Body response:")
+        print("\t - type: {}".format(type(html)))
+        print("\t - content: {}".format(html))
+        print("\t - utf8 content: {}".format(html.decode('utf8')))
