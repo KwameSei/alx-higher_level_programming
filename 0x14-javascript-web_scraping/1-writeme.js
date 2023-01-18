8a8f40682c6ce1dc14231a04f58dc8382903d584
KwameSei <nathanielosei92@gmail.com>
2023-01-18 04:56:46 +0000

    Writing to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const input = process.argv[3];
+
+fs.writeFile(file, input, 'utf-8', function (err, input) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('added');
+  }
+});
